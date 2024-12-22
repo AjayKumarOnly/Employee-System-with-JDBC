121399ff2d6efd201117b77f062229ae1b28bf43
AjayKumarOnly <ajaykumar.msaravana@gmail.com>
2024-12-22 18:18:36 +0530

    Console Based emp-sys

---
+++ src/operation_crud/Crud.java
@@ -0,0 +1,101 @@
+package operation_crud;
+
+import java.sql.*;
+
+public class Crud {
+    private static final String url ="jdbc:mysql://localhost:3306/tony";
+    private  static final String username = "root";
+    private static final String password = "";
+    public static void updateRecord(int id, String name) throws Exception{
+        String sql = "update emp set name = ? where id = ?";
+
+
+        Connection con = DriverManager.getConnection(url,username,password);
+
+        PreparedStatement pst = con.prepareStatement(sql);
+        pst.setString(1,name);
+        pst.setInt(2,id);
+
+        int rs = pst.executeUpdate();
+        System.out.println("Number of rows Updated : "+ rs);
+        pst.close();
+        con.close();
+    }
+    public static void sp(int id)throws Exception{
+
+        Connection con = DriverManager.getConnection(url,username,password);
+
+        CallableStatement cst = con.prepareCall("{call select_Id(?)}");
+        cst.setInt(1,id);
+        ResultSet rs = cst.executeQuery();
+        while(rs.next()) {
+            System.out.println("Id : " + rs.getInt(1));
+            System.out.println("Name : " + rs.getString(2));
+            System.out.println("Salary : " + rs.getInt(3));
+        }
+        cst.close();
+        con.close();
+    }
+    public static void readRecords() throws Exception{
+        String sql = "select * from emp";
+
+
+        Connection con = DriverManager.getConnection(url,username,password);
+
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+
+        while(rs.next()) {
+            System.out.println("Id : " + rs.getInt(1));
+            System.out.println("Name : " + rs.getString(2));
+            System.out.println("Salary : " + rs.getInt(3));
+        }
+        con.close();
+    }
+    public static  void insertRecord(int id,String name,int salary) throws Exception{
+        String sql = "insert into emp values(?,?,?)";
+
+
+
+        Connection con = DriverManager.getConnection(url,username,password);
+
+        PreparedStatement pst = con.prepareStatement(sql);
+        pst.setInt(1,id);
+        pst.setString(2,name);
+        pst.setInt(3,salary);
+
+        int rs = pst.executeUpdate();
+        System.out.println("Number of rows Affected : "+ rs);
+        pst.close();
+        con.close();
+    }
+    public static void deleteRecord(int id) throws Exception{
+        String sql = "delete from emp where id = ?";
+
+
+        Connection con = DriverManager.getConnection(url,username,password);
+
+        PreparedStatement pst = con.prepareStatement(sql);
+        pst.setInt(1,id);
+
+        int rs = pst.executeUpdate();
+        System.out.println("Number of rows Deleted : "+ rs);
+        pst.close();
+        con.close();
+    }
+    public static void updateRecord(int id,int upSalary)throws Exception{
+        String sql = "update emp set salary = ? where id = ?";
+
+
+        Connection con = DriverManager.getConnection(url,username,password);
+
+        PreparedStatement pst = con.prepareStatement(sql);
+        pst.setInt(1,upSalary);
+        pst.setInt(2,id);
+
+        int rs = pst.executeUpdate();
+        System.out.println("Number of rows Updated : "+ rs);
+        pst.close();
+        con.close();
+    }
+}
---
+++ src/operation_crud/Main.java
@@ -0,0 +1,66 @@
+package operation_crud;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Crud crud = new Crud();
+        Scanner scanner = new Scanner(System.in); // Open Scanner once
+
+        boolean running = true;
+        while (running) {
+            System.out.println("\nEnter the Number To Do CRUD Operation:\n");
+            System.out.println("1 -> Read table");
+            System.out.println("2 -> Insert Records");
+            System.out.println("3 -> Update Salary");
+            System.out.println("4 -> Update Name");
+            System.out.println("5 -> Delete Record by Id");
+            System.out.println("0 -> Exit");
+
+            System.out.print("\nYour Choice: ");
+            int input = scanner.nextInt();
+
+            switch (input) {
+                case 1:
+                    crud.readRecords();
+                    break;
+                case 2:
+                    System.out.print("Enter ID: ");
+                    int id = scanner.nextInt();
+                    System.out.print("Enter Name: ");
+                    String name = scanner.next();
+                    System.out.print("Enter Salary: ");
+                    int salary = scanner.nextInt();
+                    crud.insertRecord(id, name, salary);
+                    break;
+                case 3:
+                    System.out.print("Enter ID to Update Salary: ");
+                    int id1 = scanner.nextInt();
+                    System.out.print("Enter New Salary: ");
+                    int updateSalary = scanner.nextInt();
+                    crud.updateRecord(id1, updateSalary);
+                    break;
+                case 4:
+                    System.out.print("Enter ID to Update Name: ");
+                    int id2 = scanner.nextInt();
+                    System.out.print("Enter New Name: ");
+                    String updateName = scanner.next();
+                    crud.updateRecord(id2, updateName);
+                    break;
+                case 5:
+                    System.out.print("Enter ID to Delete Record: ");
+                    int deleteId = scanner.nextInt();
+                    crud.deleteRecord(deleteId);
+                    break;
+                case 0:
+                    running = false;
+                    System.out.println("Exiting Program...");
+                    break;
+                default:
+                    System.out.println("Please enter a valid number.");
+            }
+        }
+
+        scanner.close();
+    }
+}
